42bfc44058f5875ee91afa607c2912518bd217a6
Tom Burns <Tomulvlup@gmail.com>
2023-10-15 21:42:01 +0100

    Styling improvements

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,15 +1,32 @@
-// AboutMe.js
-
 import React from 'react';
-import './AboutMe.css'
-// In AboutMe.js
-export const AboutMe = ({ className }) => {
+import './AboutMe.css';
+import Brumjs from '../../assets/images/brumjslogo.webp'; // Import the first image
+import TechmidsLogo from '../../assets/images/Techmidslogo.png'; // Import the second image
+
+export const AboutMe = () => {
   return (
-      <section id="about" className={className}>
-          <h1>About Me</h1>
-          <h3>Coder & Tech Enthusiast 💻</h3>
-          <p>This is the About Me section.</p>
-      </section>
+    <section id="about" className="hidden">
+      <h1>About Me</h1>
+      <h3>Coder & Tech Enthusiast 💻</h3>
+      <p>
+        Passionate about solving problems through coding. Becoming a regular at Tech meetups such as Brum.js and with a bit of luck Techmids in October
+      </p>
+      <div className="gallery">
+        <ul>
+          <li>
+            <a href="https://www.meetup.com/brum_js/" className="img-container"> {/* Apply the CSS class */}
+              <img src={Brumjs} alt="Brum.js Logo" width="300" height="150" />
+            </a>
+          </li>
+          <li>
+            <a href="https://conf.techmids.io/" className="img-container"> {/* Apply the CSS class */}
+              <img src={TechmidsLogo} alt="Techmids Logo" width="300" height="150" />
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Rest of your "About Me" content goes here... */}
+    </section>
   );
 };
-
