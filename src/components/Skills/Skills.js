19211f2f9d429eef3ae31d905cbb81b090788076
Tom Burns <Tomulvlup@gmail.com>
2023-10-19 23:28:07 +0100

    adding some images

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -12,6 +12,10 @@ export const Skills = ({ className }) => {
       <div className="content-overlay">
         <h1>Skills</h1>
         <p>Your skills here.</p>
+        <img src="images/java-script.png"/>
+        <img src="images/html-5.png"/>
+        <img src="images/css-3.png"/>
+        <img src="images/react.png"/>
       </div>
     </section>
   );
