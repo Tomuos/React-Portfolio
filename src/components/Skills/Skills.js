03db25aca1f0bbfc0d7a93ff14bb8975e23255c0
Tom Burns <Tomulvlup@gmail.com>
2023-10-21 23:04:49 +0100

    brief descriptions but more will need to be added

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -13,9 +13,13 @@ export const Skills = ({ className }) => {
         <h1>Skills</h1>
         <p>Your skills here.</p>
         <img src="images/java-script.png"/>
+        <p>a year of Javascript</p>
         <img src="images/html-5.png"/>
+        <p>few years of html</p>
         <img src="images/css-3.png"/>
+        <p>few years of CSS</p>
         <img src="images/react.png"/>
+        <p>6 months of react</p>
       </div>
     </section>
   );
