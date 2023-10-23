a57bb1b02f68afaa69f1d46181931705b9c1e215
Tom Burns <Tomulvlup@gmail.com>
2023-10-23 13:12:57 +0100

    tidying up

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,8 +6,8 @@ export const AboutMe = () => {
   return (
     <section id="about" className="hidden">
     <div className="content">
-      <h1>About Me</h1>
       <section id="about-me" className="about-me-section">
+      <h1>About Me</h1>
       
       <div className="about-me-content">
         
@@ -16,11 +16,7 @@ export const AboutMe = () => {
             Hello, I'm Tom! I'm a developer with a focus on front-end technologies like React, HTML, and CSS. 
             I have a deep-rooted passion for coding and love taking on new challenges to expand my skill set.
           </p>
-          <p>
-            When I'm not coding, you can find me building computers, attending tech meetups like TechMids, or 
-            spending quality time outdoors with my family. I also love hosting pizza parties and gaming — 
-            the new Zelda game TOTK is my current favorite!
-          </p>
+          
           <p>
             I'm currently looking for new job opportunities where I can bring my unique blend of skills and 
             passion to a forward-thinking company.
@@ -29,18 +25,18 @@ export const AboutMe = () => {
         
         <div className="about-me-image">
   <img src="/images/Tom.jpg" alt="Tom" className="cover-image"/>
-  <img src="/images/nobgboys.png" alt="Hover Image" className="hover-image"/>
+  
 </div>
-
-        
+     
       </div>
+
     </section>
   
 
       <h3>Coder & Tech Enthusiast 💻</h3>
       <p>
         I'm a full-stack developer with a passion for creating beautiful, functional and accessible web applications. I have a keen interest in the latest technologies and enjoy learning new skills.
-      </p>
+      </p>      
       <div className="gallery">
         <ul>
           <li>
@@ -77,6 +73,13 @@ export const AboutMe = () => {
           </div>
         </div>
           <p>The boys love the outdoors and occasionally they become right posers</p>
+        </div>
+
+        <div>
+        <h3>Hobbies and Funtimes</h3>
+        <p>
+       I also love hosting pizza parties and gaming — the new Zelda game TOTK is my current favorite!
+          </p>
         </div>
       </div>
     </section>
