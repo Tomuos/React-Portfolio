6affb626293009428660fa06faf763ee72501376
Tom Burns <Tomulvlup@gmail.com>
2023-10-25 23:02:30 +0100

    Added some framer motion but it needs some work

--- src/components/MyProjects/MyProjects.js
+++ src/components/MyProjects/MyProjects.js
@@ -5,15 +5,19 @@ import './MyProjects.css';
 export const MyProjects = ({ className }) => {
   const projects = [
     {
-      id: '1',
-      title: 'Kindr',
-      description: 'I worked with a wonderful group of people at the School of Code to create a community-driven app called Kindr.',
-      githubLink: 'https://github.com/jsonandthearguments/kindr',
-      appLink: 'https://kindr.netlify.app/',
-      videoLink: 'https://github.com/Tomuos/Tomuos/assets/110429174/14af227d-5a52-4cb9-9636-5d1de81f07af'
+      id: "kindr",
+      title: "Kindr",
+      description: "I worked with a wonderful group of people at the School of Code to create a community-driven app called Kindr.",
+      links: [
+        { type: "github", url: "https://github.com/jsonandthearguments/kindr" },
+        { type: "app", url: "https://kindr.netlify.app/" },
+        { type: "presentation", url: "https://github.com/Tomuos/Tomuos/assets/110429174/14af227d-5a52-4cb9-9636-5d1de81f07af" }
+      ],
+      image: "images/bg1.png"
     }
+    // ... Add other projects in a similar manner if needed
   ];
-
+  
   const [selectedId, setSelectedId] = useState(null);
   const [selectedProject, setSelectedProject] = useState(null);
 
@@ -22,18 +26,22 @@ export const MyProjects = ({ className }) => {
       <h1>Projects</h1>
       
       <div>
-        {projects.map((project) => (
-          <motion.div
-            key={project.id}
-            layoutId={project.id}
-            onClick={() => {
-              setSelectedId(project.id);
-              setSelectedProject(project);
-            }}
-          >
-            <motion.h3>{project.title}</motion.h3>
-          </motion.div>
-        ))}
+      {projects.map(project => (
+  <motion.div 
+      key={project.id} 
+      layoutId={project.id} 
+      onClick={() => {
+        setSelectedId(project.id);
+        setSelectedProject(project);
+      }}
+      className="motion-div"
+  >
+    <motion.img src={project.image} alt={project.title} />
+    <motion.h5>{project.subtitle}</motion.h5>
+  </motion.div>
+))}
+
+
       </div>
 
       <AnimatePresence>
@@ -41,10 +49,10 @@ export const MyProjects = ({ className }) => {
           <motion.div layoutId={selectedId}>
             <motion.h3>{selectedProject.title}</motion.h3>
             <motion.p>{selectedProject.description}</motion.p>
-            <motion.a href={selectedProject.githubLink}>GitHub</motion.a>
-            <motion.a href={selectedProject.appLink}>App Link</motion.a>
-            <motion.a href={selectedProject.videoLink}>Video Presentation</motion.a>
-            <motion.button onClick={() => setSelectedId(null)}>Close</motion.button>
+            <motion.a href={selectedProject.links.find(link => link.type === "github").url}>GitHub</motion.a>
+            <motion.a href={selectedProject.links.find(link => link.type === "app").url}>App Link</motion.a>
+            <motion.a href={selectedProject.links.find(link => link.type === "presentation").url}>Video Presentation</motion.a>
+            <motion.button className="motion-button" onClick={() => setSelectedId(null)}>Close</motion.button>
           </motion.div>
         )}
       </AnimatePresence>
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -11,15 +11,15 @@ export const Skills = ({ className }) => {
     <section id="skills" className={className}>
       <div className="content-overlay">
         <h1>Skills</h1>
-        <p>Your skills here.</p>
+        
         <img src="images/java-script.png"/>
-        <p>a year of Javascript</p>
+        <h3>A year of Javascript</h3>
         <img src="images/html-5.png"/>
-        <p>few years of html</p>
+        <h3>Few years of html</h3>
         <img src="images/css-3.png"/>
-        <p>few years of CSS</p>
+        <h3>Few years of CSS</h3>
         <img src="images/react.png"/>
-        <p>6 months of react</p>
+        <h3>6 months of react</h3>
       </div>
     </section>
   );
