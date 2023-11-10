e28d073f7d61d41cd5c2a36666bbee891a131478
Tom Burns <Tomulvlup@gmail.com>
2023-11-10 16:08:24 +0000

    new image added and some styling and code clean up

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -11,15 +11,24 @@ export const Skills = ({ className }) => {
     <section id="skills" className={className}>
       <div className="content-overlay">
         <h1>Skills</h1>
-        
-        <img src="images/java-script.png" alt="JS Logo"/>
-        <h3>A year of Javascript</h3>
-        <img src="images/html-5.png" alt="HTML Logo"/>
-        <h3>Few years of html</h3>
-        <img src="images/css-3.png" alt="CSS Logo"/>
-        <h3>Few years of CSS</h3>
-        <img src="images/react.png" alt="React Logo"/>
-        <h3>6 months of react</h3>
+        <div className="skills-icons"> {/* Container for icons */}
+          <div className="skill">
+            <img src="images/java-script.png" alt="JS Logo"/>
+            <h3>A year of Javascript</h3>
+          </div>
+          <div className="skill">
+            <img src="images/html-5.png" alt="HTML Logo"/>
+            <h3>Few years of html</h3>
+          </div>
+          <div className="skill">
+            <img src="images/css-3.png" alt="CSS Logo"/>
+            <h3>Few years of CSS</h3>
+          </div>
+          <div className="skill">
+            <img src="images/react.png" alt="React Logo"/>
+            <h3>6 months of react</h3>
+          </div>
+        </div>
       </div>
     </section>
   );
