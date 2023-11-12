4385862747c106cd3c40b5e03730a2bc0f10f8e1
Tom Burns <Tomulvlup@gmail.com>
2023-11-12 23:04:05 +0000

    new wallpapers added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,63 @@
-
-import './Contact.css'
+import React, { useState } from 'react';
+import './Contact.css';
 
 export const Contact = ({ className }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you would handle the form submission.
+    // For instance, sending the data to your backend or a third-party email service.
+    console.log(formData); // For now, we'll just log the data to the console.
+  };
+
   return (
     <section id="contact" className={className}>
       <h1>Contact</h1>
       <p>Here is my contact info and Resume</p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
     </section>
   );
 };
+
+export default Contact;
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,25 +4,26 @@ import './Skills.css';
 export const Skills = () => {
   return (
     <section id="skills">
+    <div className='white-block'>
       <div className="content-overlay">
         <h1>Skills</h1>
         <h2>Front End</h2>
         <div className="skills-icons front-end-skills"> {/* Container for front-end skills icons */}
           <div className="skill">
+            <img src="images/react.png" alt="React Logo"/>
+            <h4>6 months of React</h4>
+          </div>
+          <div className="skill">
             <img src="images/java-script.png" alt="JavaScript Logo"/>
-            <h3>A year of Javascript</h3>
+            <h4>A year of Javascript</h4>
           </div>
           <div className="skill">
             <img src="images/html-5.png" alt="HTML5 Logo"/>
-            <h3>Few years of HTML</h3>
+            <h4>Few years of HTML</h4>
           </div>
           <div className="skill">
             <img src="images/css-3.png" alt="CSS3 Logo"/>
-            <h3>Few years of CSS</h3>
-          </div>
-          <div className="skill">
-            <img src="images/react.png" alt="React Logo"/>
-            <h3>6 months of React</h3>
+            <h4>Few years of CSS</h4>
           </div>
         </div>
       </div>
@@ -32,19 +33,19 @@ export const Skills = () => {
         <div className="skills-icons design-skills"> {/* Container for design skills icons */}
           <div className="skill">
             <img src="images/adobe.png" alt="Adobe Suite Logo"/>
-            <h3>Adobe Suite</h3>
+            <h4>Adobe Suite</h4>
           </div>
           <div className="skill">
             <img src="images/icons8-affinity-designer-240.png" alt="Affinity Designer Logo"/>
-            <h3>Affinity Designer</h3>
+            <h4>Affinity Designer</h4>
           </div>
           <div className="skill">
             <img src="images/icons8-affinity-photo-240.png" alt="Affinity PhotoLogo"/>
-            <h3>Affinity Photo</h3>
+            <h4>Affinity Photo</h4>
           </div>
           <div className="skill">
             <img src="images/icons8-affinity-publisher-240.png" alt="Affinity Publisher Logo"/>
-            <h3>Affinity Publisher</h3>
+            <h4>Affinity Publisher</h4>
           </div>
         </div>
       </div>
@@ -54,15 +55,15 @@ export const Skills = () => {
         <div className="skills-icons other-tools-skills"> {/* Container for other tools skills icons */}
           <div className="skill">
             <img src="images/icons8-docker-240.png" alt="Docker Logo"/>
-            <h3>Docker</h3>
+            <h4>Docker</h4>
           </div>
           <div className="skill">
             <img src="images/icons8-git-240.png" alt="Git Logo"/>
-            <h3>Git</h3>
+            <h4>Git</h4>
           </div>
           <div className="skill">
             <img src="images/trello.png" alt="Trello Logo"/>
-            <h3>Trello</h3>
+            <h4>Trello</h4>
           </div>
         </div>
       </div>
@@ -71,15 +72,15 @@ export const Skills = () => {
         <div className="skills-icons photography-skills"> {/* Container for photography skills icons */}
           <div className="skill">
             <img src="images/camera.png" alt="Photography Logo"/>
-            <h3>Photography</h3>
+            <h4>Photography</h4>
           </div>
           <div className="skill">
             <img src="images/icons8-figma-240.png" alt="Figma Logo"/>
-            <h3>Figma</h3>
+            <h4>Figma</h4>
           </div>
         </div>
       </div>
-
+</div>
     </section>
   );
 };
